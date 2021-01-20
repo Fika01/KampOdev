52bcc9536d29279ac814020d6634b866a94b83b2
Fika01 <55555474+Fika01@users.noreply.github.com>
2021-01-20 21:09:00 +0300

    Add project files.

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string TcNo { get; set; }
+        public string Meslek { get; set; }
+
+    }
+}
